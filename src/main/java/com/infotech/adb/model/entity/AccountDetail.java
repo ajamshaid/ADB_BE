5faead6d1d95fd58a25efa59d08b0f6c6f8d55fd
Ajam <ajam0014@gmail.com>
2021-05-07 15:15:35 +0500

    SILK bank Integration start

--- src/main/java/com/infotech/adb/model/entity/AccountDetail.java
+++ src/main/java/com/infotech/adb/model/entity/AccountDetail.java
@@ -1,8 +1,8 @@
 package com.infotech.adb.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Getter;
-import lombok.Setter;
+import com.opencsv.bean.CsvBindByName;
+import lombok.Data;
 
 import javax.persistence.*;
 import java.util.Set;
@@ -10,31 +10,47 @@ import java.util.Set;
 @Entity
 @Table(name = "ACCOUNT_DETAIL")
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Getter
-@Setter
+@Data
 public class AccountDetail extends BaseEntity {
 
+
+    @CsvBindByName(column = "\uFEFFIBAN")
+  //  @CsvBindByPosition(position = 0)
     @Column(name = "IBAN", unique = true, nullable = false)
     private String iban;
 
+    @CsvBindByName(column = "Email")
     @Column(name = "EMAIL")
     private String email;
 
+    @CsvBindByName(column = "MobileNumber")
     @Column(name = "MOBILE_NUMBER")
     private String mobileNumber;
 
+    @CsvBindByName(column = "Account Title")
     @Column(name = "ACCT_TITLE")
     private String accountTitle;
+
+    @CsvBindByName(column = "Account Number")
     @Column(name = "ACCT_NUMBER")
     private String accountNumber;
+
+    @CsvBindByName(column = "Account Status")
     @Column(name = "ACCT_STATUS")
     private String accountStatus;
+
+    @CsvBindByName(column = "Account Type")
     @Column(name = "ACCT_TYPE")
     private String accountType;
+
+    @CsvBindByName(column = "NTN")
     @Column(name = "NTN")
     private String ntn;
+
+    @CsvBindByName(column = "CNIC")
     @Column(name = "CNIC")
     private String cnic;
+
 
     @OneToMany(mappedBy = "accountDetail", fetch = FetchType.LAZY)
     private Set<AuthorizedPaymentModes> authorizedPaymentModesSet;
@@ -47,4 +63,15 @@ public class AccountDetail extends BaseEntity {
 
     @OneToMany(mappedBy = "accountDetail", fetch = FetchType.LAZY)
     private Set<RestrictedSuppliers> restrictedSuppliersSet;
+
+
+    @CsvBindByName(column = "AuthPM(IM)")
+    private String AuthPMImport;
+    @CsvBindByName(column = "AuthPM(EX)")
+    private String AuthPMExport;
+    @CsvBindByName(column = "Restricted Countries(IM)")
+    private String restrictedCountriesImport;
+    @CsvBindByName(column = "Restricted Countries(EX)")
+    private String restrictedCountriesExport;
+
 }
--- src/main/java/com/infotech/adb/util/TestMain.java
+++ src/main/java/com/infotech/adb/util/TestMain.java
@@ -1,10 +1,41 @@
 package com.infotech.adb.util;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.infotech.adb.model.entity.AccountDetail;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class TestMain {
 
-    public static void main(String args[]){
-        System.out.println(new BCryptPasswordEncoder().encode("admin1"));
+    public static void main(String args[]) throws CsvException, FileNotFoundException {
+
+      //  System.out.println(new BCryptPasswordEncoder().encode("admin1"));
+
+        String fileName = "D:\\InfoTech\\ADBroker\\SilkBank\\TP Upload Sheet.csv";
+        try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+            List<String[]> r = reader.readAll();
+            r.forEach(x -> System.out.println("row---"+Arrays.toString(x)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+        List<AccountDetail> beans =
+                new CsvToBeanBuilder<AccountDetail>(new FileReader(fileName))
+                .withType(AccountDetail.class)
+                .build().parse();
+
+        beans.forEach(System.out::println);
+
+
     }
 }
